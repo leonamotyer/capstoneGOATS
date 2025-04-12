b46b289ae19404e05bbae1b5b30c799cd9f9910a
L. Motyer <158305618+leonamotyer@users.noreply.github.com>
2025-04-11 18:08:09 -0600

    styling

--- app/layout.js
+++ app/layout.js
@@ -18,8 +18,7 @@ export default function RootLayout({ children }) {
                   className="rounded-full shadow-sm"
                   style={{ width: '64px', height: '64px' }} // Explicitly set the size
                 />
-                <h1 className="text-lg font-semibold">YYC Food Trucks</h1>
-              </div>
+                <h1 className="text-lg font-semibold">YYC Food Trucks</h1>              </div>
               <nav className="nav-links flex gap-4">
                 <a href="/schedule" className="nav-link flex items-center gap-1 hover:text-yellow-300">
                   <FiCalendar /> Schedule
